d0df69c57f4935c6fd0b5bf0abfbda6739450a3c
Anton Cheng <antonassocareer@gmail.com>
2024-10-07 14:18:07 +0800

    chore: simple template with morpho base test

---
+++ src/interfaces/IMonarchAgent.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+interface IMonarchAgent {
+    event RebalancerSet(address indexed user, address indexed rebalancer);
+}
---
+++ test/unit-tests/Basic.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MonarchAgentV1} from "../../src/agents/MonarchAgent.sol";
+import {BaseTest} from "morpho-blue/test/forge/BaseTest.sol";
+
+contract AgentTest is BaseTest {
+    MonarchAgentV1 public agent;
+
+    function setUp() public override {
+        super.setUp();
+        agent = new MonarchAgentV1(address(morpho));
+    }
+
+    function test_SetRebalancer() public {
+        address user = address(0x1);
+        address rebalancer = address(0x2);
+
+        vm.prank(user);
+        agent.authorize(rebalancer);
+
+        assertEq(agent.rebalancers(user), rebalancer);
+    }
+}
