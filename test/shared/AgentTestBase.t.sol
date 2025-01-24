e490e71bfb30a9461d7722d5605842f82bcc814b
Shao <44807495+tienshaoku@users.noreply.github.com>
2025-01-25 00:13:07 +0800

    Feat/audit report and test (#12)
    * test: one test for approval depleted
    
    * docs: audit report v1

--- test/shared/AgentTestBase.t.sol
+++ test/shared/AgentTestBase.t.sol
@@ -5,6 +5,7 @@ import {MonarchAgentV1} from "../../src/agents/MonarchAgent.sol";
 import {BaseTest} from "morpho-blue/test/forge/BaseTest.sol";
 import {MarketParamsLib, Id} from "morpho-blue/src/libraries/MarketParamsLib.sol";
 import {MarketParams} from "morpho-blue/src/interfaces/IMorpho.sol";
+import {SafeTransferLib, ERC20} from "solmate/src/utils/SafeTransferLib.sol";
 
 contract AgentTestBase is BaseTest {
     using MarketParamsLib for MarketParams;
@@ -32,6 +33,22 @@ contract AgentTestBase is BaseTest {
     function _createAndEnableMarket(address user, uint256 lltv) internal returns (MarketParams memory market) {
         market = _createMarket(lltv);
         _enableMarket(user, market);
+    }
+
+    function _createAndEnableMarketFake(address user, uint256 lltv, address fakeAgent)
+        internal
+        returns (MarketParams memory market)
+    {
+        market = _createMarket(lltv);
+
+        vm.startPrank(user);
+        bytes32 marketId = Id.unwrap(market.id());
+        bytes32[] memory markets = new bytes32[](1);
+        uint256[] memory caps = new uint256[](1);
+        markets[0] = marketId;
+        caps[0] = type(uint256).max;
+        FakeAgent(fakeAgent).batchConfigMarkets(markets, caps);
+        vm.stopPrank();
     }
 
     function _setMarketCap(address user, MarketParams memory market, uint256 cap) internal {
@@ -62,5 +79,15 @@ contract AgentTestBase is BaseTest {
         vm.startPrank(user);
         agent.authorize(rebalancer);
         vm.stopPrank();
+    }
+}
+
+contract FakeAgent is MonarchAgentV1 {
+    using SafeTransferLib for ERC20;
+
+    constructor(address morphoBlue) MonarchAgentV1(morphoBlue) {}
+
+    function approve(address asset, address spender) public {
+        ERC20(asset).safeApprove(spender, 1);
     }
 }
